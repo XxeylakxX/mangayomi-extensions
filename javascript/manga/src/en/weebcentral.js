f80e8e8c94c55793a784d1af8d0b12d13a71e027
XxeylakxX <99536219+XxeylakxX@users.noreply.github.com>
2026-01-10 08:54:49 +0800

    Update weebcentral.js

--- javascript/manga/src/en/weebcentral.js
+++ javascript/manga/src/en/weebcentral.js
@@ -7,7 +7,7 @@ const mangayomiSources = [{
     "iconUrl": "https://www.google.com/s2/favicons?sz=128&domain=https://weebcentral.com",
     "typeSource": "single",
     "itemType": 0,
-    "version": "0.1.9",
+    "version": "0.1.0",
     "pkgPath": "manga/src/en/weebcentral.js"
 }];
 
@@ -16,152 +16,124 @@ class DefaultExtension extends MProvider {
         super();
         this.client = new Client();
     }
-
     getHeaders(url) {
         return { "Referer": `${this.source.baseUrl}/` };
     }
 
     async request(slug) {
-        const url = `${this.source.baseUrl}${slug}`;
-        const res = await this.client.get(url);
+        var url = `${this.source.baseUrl}${slug}`
+        var res = await this.client.get(url);
         return new Document(res.body);
     }
+
 
     async getPopular(page) {
         const filters = this.getFilterList();
         filters[0].state = 2;
-        return await this.search("", page, filters);
+        return await this.search("", page, filters)
     }
 
     async getLatestUpdates(page) {
         const filters = this.getFilterList();
         filters[0].state = 5;
-        return await this.search("", page, filters);
+        return await this.search("", page, filters)
     }
+
+    getImageUrl(id) { return `https://temp.compsci88.com/cover/normal/${id}.webp`; }
 
     async search(query, page, filters) {
-        const safeFilters = Array.isArray(filters) ? filters : this.getFilterList();
-        const offset = 32 * (parseInt(page) - 1);
-
-        const sort = encodeURIComponent(safeFilters[0].values[safeFilters[0].state].value);
-        const order = encodeURIComponent(safeFilters[1].values[safeFilters[1].state].value);
-        const translation = encodeURIComponent(safeFilters[2].values[safeFilters[2].state].value);
-        const text = encodeURIComponent(query ?? "");
-
-        let status = "";
-        for (const f of safeFilters[3].state) {
-            if (f.state) status += `&included_status=${f.value}`;
+        var offset = 32 * (parseInt(page) - 1)
+        var sort = filters[0].values[filters[0].state].value
+        var order = filters[1].values[filters[1].state].value
+        var translation = filters[2].values[filters[2].state].value
+        var status = ""
+        for (var filter of filters[3].state) {
+            if (filter.state == true)
+                status += `&included_status=${filter.value}`
+        }
+        var type = ""
+        for (var filter of filters[4].state) {
+            if (filter.state == true)
+                type += `&included_type=${filter.value}`
+        }
+        var tags = ""
+        for (var filter of filters[5].state) {
+            if (filter.state == true)
+                tags += `&included_tag=${filter.value}`
+        }
+        var slug = `/search/data?limit=32&offset=${offset}&author=&text=${query}&sort=${sort}&order=${order}&official=${translation}${status}${type}${tags}&display_mode=Full%20Display`
+        var doc = await this.request(slug);
+        var list = [];
+        var mangaElements = doc.select("article:has(section)")
+        for (var manga of mangaElements) {
+            var imageUrl = manga.selectFirst("img").getSrc;
+            var details = manga.selectFirst("section > a");
+            var link = details.getHref;
+            var name = manga.selectFirst("article > div > div > div").text;
+            list.push({ name, imageUrl, link });
         }
 
-        let type = "";
-        for (const f of safeFilters[4].state) {
-            if (f.state) type += `&included_type=${f.value}`;
-        }
+        var hasNextPage = doc.selectFirst("button").text.length > 0;
+        return { list, hasNextPage }
 
-        let tags = "";
-        for (const f of safeFilters[5].state) {
-            if (f.state) tags += `&included_tag=${f.value}`;
-        }
-
-        const slug = `/search/data?limit=32&offset=${offset}&author=&text=${text}&sort=${sort}&order=${order}&official=${translation}${status}${type}${tags}&display_mode=Full%20Display`;
-        const doc = await this.request(slug);
-
-        const list = [];
-        const mangaElements = doc.select("article:has(section)");
-        
-        for (const manga of mangaElements) {
-            const img = manga.selectFirst("img");
-            const a = manga.selectFirst("section > a");
-            
-            list.push({
-                name: manga.selectFirst("article > div > div > div")?.text ?? "",
-                imageUrl: img ? img.attr("src") : "", // FIXED: Use attr("src")
-                link: a ? a.attr("href") : ""         // FIXED: Use attr("href")
-            });
-        }
-
-        const hasNextPage = doc.selectFirst("button:not([disabled])") != null;
-        return { list, hasNextPage };
     }
-
     statusCode(status) {
-        switch ((status ?? "").toLowerCase()) {
-            case "ongoing": return 0;
-            case "complete": return 1;
-            case "hiatus": return 2;
-            case "canceled": return 3;
-            default: return 5;
-        }
+        return {
+            "Ongoing": 0,
+            "Complete": 1,
+            "Hiatus": 2,
+            "Canceled": 3,
+        }[status] ?? 5;
     }
 
     async getDetail(url) {
-        const clean = url.replace(/^https?:\/\/[^/]+/, "");
-        const slug = clean.startsWith("/series/") ? clean : `/series/${clean}`;
-        
-        let doc = await this.request(slug);
+        var urlSplits = url.split("/");
+        var link = urlSplits[urlSplits.length - 2];
+        var slug = url.startsWith("http") ? `/series/${link}` : `/series/${url}`;
+        var doc = await this.request(slug);
+        var imageUrl = url.startsWith("http") ? this.getImageUrl(link) : this.getImageUrl(url);
+        var description = doc.selectFirst("p.whitespace-pre-wrap.break-words").text
 
-        // FIXED: Scrape the actual image from the detail page instead of generating a temp URL
-        const imgEl = doc.selectFirst("img.object-cover");
-        const imageUrl = imgEl ? imgEl.attr("src") : ""; 
-        
-        const description = doc.selectFirst("p.whitespace-pre-wrap.break-words")?.text ?? "";
-
-        let chapters = [];
-        let author = "";
-        let genre = [];
-        let status = 5;
-
-        const ul = doc.select("ul.flex.flex-col.gap-4 > li");
-        for (const li of ul) {
-            const strongTxt = li.selectFirst("strong")?.text ?? "";
-            if (strongTxt.includes("Author(s):")) {
-                author = li.selectFirst("a")?.text ?? "";
-            } else if (strongTxt.includes("Tags(s):")) {
-                li.select("a").forEach(a => genre.push(a.text));
-            } else if (strongTxt.includes("Status:")) {
-                status = this.statusCode(li.selectFirst("a")?.text ?? "");
+        var chapters = []
+        var ul = doc.select("ul.flex.flex-col.gap-4 > li")
+        var author = ""
+        var genre = []
+        var status = 5
+        for (var li of ul) {
+            var strongTxt = li.selectFirst("strong").text
+            if (strongTxt.indexOf("Author(s):") != -1) {
+                author = li.selectFirst("a").text
+            } else if (strongTxt.indexOf("Tags(s):") != -1) {
+                li.select("a").forEach(a => genre.push(a.text))
+            } else if (strongTxt.indexOf("Status:") != -1) {
+                status = this.statusCode(li.selectFirst("a").text)
             }
+
         }
 
-        // Fetch Chapters
-        const chapSlug = `${slug}/full-chapter-list`;
+        var chapSlug = `${slug}/full-chapter-list`
         doc = await this.request(chapSlug);
-
-        const chapList = doc.select("div.flex.items-center");
-        for (const chap of chapList) {
-            const nameEl = chap.selectFirst("span.grow.flex.items-center.gap-2")?.selectFirst("span");
-            const name = nameEl ? nameEl.text : "";
-            
-            const timeEl = chap.selectFirst("time.text-datetime");
-            const dateUpload = timeEl 
-                ? new Date(timeEl.attr("datetime") ?? timeEl.text).valueOf().toString() 
-                : "0";
-                
-            const inputEl = chap.selectFirst("input");
-            const url = inputEl ? inputEl.attr("value") : "";
-            
-            chapters.push({ name, url, dateUpload });
+        var chapList = doc.select("div.flex.items-center");
+        for (var chap of chapList) {
+            var name = chap.selectFirst("span.grow.flex.items-center.gap-2").selectFirst("span").text
+            var dateUpload = new Date(chap.selectFirst("time.text-datetime").text).valueOf().toString()
+            var url = chap.selectFirst("input").attr("value")
+            chapters.push({ name, url, dateUpload })
         }
+        return { description, imageUrl, author, genre, status, chapters }
 
-        return { description, imageUrl, author, genre, status, chapters };
     }
-
     async getPageList(url) {
-        const slug = `/chapters/${url}/images?current_page=1&reading_style=long_strip`;
-        const doc = await this.request(slug);
+        var slug = `/chapters/${url}/images?current_page=1&reading_style=long_strip`
+        var doc = await this.request(slug);
 
-        const urls = [];
-        doc.select("section > img").forEach(page => urls.push(page.attr("src")));
+        var urls = [];
 
-        return urls.map(x => ({
-            url: x,
-            headers: {
-                Referer: `${this.source.baseUrl}/`,
-                Accept: "image/avif,image/webp,*/*",
-                Host: `${x.match(/^(?:https?:\/\/)?([^\/:]+)(:\d+)?/)[1]}`
-            }
-        }));
+        doc.select("section > img").forEach(page => urls.push(page.attr("src")))
+
+        return urls.map(x => ({ url: x, headers: { Referer: `${this.source.baseUrl}/`, Accept: "image/avif,image/webp,*/*", Host: `${x.match(/^(?:https?:\/\/)?([^\/:]+)(:\d+)?/)[1]}` } }));
     }
+
 
     getFilterList() {
         return [
@@ -177,8 +149,7 @@ class DefaultExtension extends MProvider {
                     ["Recently Added", "Recently Added"],
                     ["Latest Updates", "Latest Updates"]
                 ].map(x => ({ type_name: 'SelectOption', name: x[0], value: x[1] }))
-            },
-            {
+            }, {
                 type_name: "SelectFilter",
                 name: "Order",
                 state: 0,
@@ -186,51 +157,76 @@ class DefaultExtension extends MProvider {
                     ["Ascending", "Ascending"],
                     ["Descending", "Descending"]
                 ].map(x => ({ type_name: 'SelectOption', name: x[0], value: x[1] }))
-            },
-            {
+            }, {
                 type_name: "SelectFilter",
                 name: "Official Translation",
                 state: 0,
                 values: [
                     ["Any", "Any"],
                     ["True", "True"],
-                    ["False", "False"]
+                    ["False", "False"],
                 ].map(x => ({ type_name: 'SelectOption', name: x[0], value: x[1] }))
-            },
-            {
+            }, {
                 type_name: "GroupFilter",
                 name: "Series Status",
                 state: [
                     ["Ongoing", "Ongoing"],
                     ["Complete", "Complete"],
                     ["Hiatus", "Hiatus"],
-                    ["Canceled", "Canceled"]
+                    ["Canceled", "Canceled"],
                 ].map(x => ({ type_name: 'CheckBox', name: x[0], value: x[1] }))
-            },
-            {
+            }, {
                 type_name: "GroupFilter",
                 name: "Series Type",
                 state: [
                     ["Manga", "Manga"],
                     ["Manhwa", "Manhwa"],
                     ["Manhua", "Manhua"],
-                    ["OEL", "OEL"]
+                    ["OEL", "OEL"],
                 ].map(x => ({ type_name: 'CheckBox', name: x[0], value: x[1] }))
-            },
-            {
+            }, {
                 type_name: "GroupFilter",
                 name: "Tags",
                 state: [
-                    ["Action", "Action"], ["Adventure", "Adventure"], ["Adult", "Adult"], ["Comedy", "Comedy"],
-                    ["Doujinshi", "Doujinshi"], ["Drama", "Drama"], ["Ecchi", "Ecchi"], ["Fantasy", "Fantasy"],
-                    ["Gender Bender", "Gender Bender"], ["Harem", "Harem"], ["Hentai", "Hentai"], ["Historical", "Historical"],
-                    ["Horror", "Horror"], ["Isekai", "Isekai"], ["Josei", "Josei"], ["Lolicon", "Lolicon"], ["Martial Arts", "Martial Arts"],
-                    ["Mature", "Mature"], ["Mecha", "Mecha"], ["Mystery", "Mystery"], ["Psychological", "Psychological"], ["Romance", "Romance"],
-                    ["School Life", "School Life"], ["Sci-Fi", "Sci-Fi"], ["Seinen", "Seinen"], ["Shotacon", "Shotacon"], ["Shoujo", "Shoujo"],
-                    ["Shoujo Ai", "Shoujo Ai"], ["Shounen", "Shounen"], ["Slice of Life", "Slice of Life"], ["Smut", "Smut"], ["Sports", "Sports"],
-                    ["Supernatural", "Supernatural"], ["Tragedy", "Tragedy"], ["Yaoi", "Yaoi"], ["Yuri", "Yuri"], ["Other", "Other"]
+                    ["Action", "Action"],
+                    ["Adventure", "Adventure"],
+                    ["Adult", "Adult"],
+                    ["Comedy", "Comedy"],
+                    ["Doujinshi", "Doujinshi"],
+                    ["Drama", "Drama"],
+                    ["Ecchi", "Ecchi"],
+                    ["Fantasy", "Fantasy"],
+                    ["Gender Bender", "Gender Bender"],
+                    ["Harem", "Harem"],
+                    ["Hentai", "Hentai"],
+                    ["Historical", "Historical"],
+                    ["Horror", "Horror"],
+                    ["Isekai", "Isekai"],
+                    ["Josei", "Josei"],
+                    ["Lolicon", "Lolicon"],
+                    ["Martial Arts", "Martial Arts"],
+                    ["Mature", "Mature"],
+                    ["Mecha", "Mecha"],
+                    ["Mystery", "Mystery"],
+                    ["Psychological", "Psychological"],
+                    ["Romance", "Romance"],
+                    ["School Life", "School Life"],
+                    ["Sci-Fi", "Sci-Fi"],
+                    ["Seinen", "Seinen"],
+                    ["Shotacon", "Shotacon"],
+                    ["Shoujo", "Shoujo"],
+                    ["Shoujo Ai", "Shoujo Ai"],
+                    ["Shounen", "Shounen"],
+                    ["Slice of Life", "Slice of Life"],
+                    ["Smut", "Smut"],
+                    ["Sports", "Sports"],
+                    ["Supernatural", "Supernatural"],
+                    ["Tragedy", "Tragedy"],
+                    ["Yaoi", "Yaoi"],
+                    ["Yuri", "Yuri"],
+                    ["Other", "Other"]
                 ].map(x => ({ type_name: 'CheckBox', name: x[0], value: x[1] }))
             }
-        ];
+        ]
     }
 }
